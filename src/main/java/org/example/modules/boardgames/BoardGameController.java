2d3190f78ba362a97795d264f6127186b87c369c
mykola-derevianko <derevianko.work@gmail.com>
2025-10-27 14:27:27 +0100

    fix: renamed boardgames controller root path + rewrote postman tests with new root

--- src/main/java/org/example/modules/boardgames/BoardGameController.java
+++ src/main/java/org/example/modules/boardgames/BoardGameController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/board-games")
+@RequestMapping("/boardgames")
 public class BoardGameController {
 
     private final BoardGameService service;
